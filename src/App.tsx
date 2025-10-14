a481d28828992d033ed5e5de33fcaf87373e90a6
Deniz <auo@tuta.io>
2025-10-14 18:53:36 +0200

    Created preliminary file structure.

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,25 @@
 import './App.css'
+import { BrowserRouter, Routes, Route, } from 'react-router'
+
+import Home from './assets/pages/Home'
+import About from './assets/pages/About'
+import Layout from './assets/components/Layout'
 
 function App() {
 
   return (
-    <>
-    hello
-    </>
+    <BrowserRouter>
+      <Routes>
+
+        <Route path="/" element={<Layout />} >
+
+          <Route index element={<Home />} />
+          <Route path="/about" element={<About />} />
+
+        </Route>
+
+      </Routes>
+    </BrowserRouter>
   )
 }
 
