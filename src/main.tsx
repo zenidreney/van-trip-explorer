af48de83b24b5a977e03aff29bb8ad38d9871e3b
Deniz <auo@tuta.io>
2025-11-03 17:22:01 +0100

    added react bootstrap dependency. added carousel test in about page

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,7 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 /* import { getApiObject, getVan } from './api.ts'
 
