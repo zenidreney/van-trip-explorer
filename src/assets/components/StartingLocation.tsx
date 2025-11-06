2190c4c1cba1f39e9f7beed156b9ae1f858112d5
Deniz <auo@tuta.io>
2025-11-06 20:12:24 +0100

    created StartingLocation component

---
+++ src/assets/components/StartingLocation.tsx
@@ -0,0 +1,75 @@
+import { getCoordinates } from "../../utils/getCoordinates";
+import Button from 'react-bootstrap/Button';
+import Form from 'react-bootstrap/Form';
+import Stack from 'react-bootstrap/Stack';
+import { useEffect, useRef } from "react";
+import { useLocation } from "../hooks/useLocation";
+
+
+export default function StartingLocation() {
+
+    const inputRef = useRef<HTMLInputElement>(null)
+
+    const { startLocation, setStartLocation } = useLocation()
+
+    async function handleStartSubmit(e: React.FormEvent<HTMLFormElement>) {
+        e.preventDefault()
+
+        console.log(inputRef.current?.value)
+
+        const startPoint = inputRef.current && inputRef.current.value ? inputRef.current.value : "madrid"
+
+
+
+        const { name, latitude, longitude } = await getCoordinates(startPoint)
+        //console.log(name, longitude, latitude)
+        setStartLocation({
+            location: name,
+            lat: latitude,
+            long: longitude
+        })
+
+    }
+
+
+
+    useEffect(() => {
+        const { location, lat, long } = startLocation
+
+        console.log("City:", location, "latitude", lat, "longitude", long);
+    }, [startLocation]);
+
+    const { location, lat, long } = startLocation
+
+    console.log("2nd console", location, lat, long)
+
+
+
+    return(
+        <Form onSubmit={handleStartSubmit}>
+                        <Form.Group className="mb-3" controlId="formStartLocation">
+                            <Form.Label>Starting Point</Form.Label>
+                            <Form.Control
+                                ref={inputRef}
+                                type="text"
+                                placeholder="Enter start location"
+                                required />
+                            <Form.Text className="text-muted">
+                                I want to start my journey from...
+                            </Form.Text>
+                        </Form.Group>
+
+
+
+                        <Stack direction="horizontal" className="align-items-end justify-content-between">
+                            <Button variant="primary" type="submit">
+                                Submit
+                            </Button>
+                            {
+                                location ? <p className="m-0">Start from: {location}</p> : ""
+                            }
+                        </Stack>
+                    </Form>
+    )
+}
+
--- src/assets/pages/VanDetail.tsx
+++ src/assets/pages/VanDetail.tsx
@@ -1,56 +1,20 @@
 import { useParams } from "react-router"
 import { useVan } from "../hooks/useVan"
 import Map from "../components/Map"
-import Button from 'react-bootstrap/Button';
-import Form from 'react-bootstrap/Form';
+
 import Stack from 'react-bootstrap/Stack';
 import Image from 'react-bootstrap/Image';
 
-
-import { getCoordinates } from "../../utils/getCoordinates";
-
-import { useEffect, useRef } from "react";
 import { useLocation } from "../hooks/useLocation";
+
+import StartingLocation from "../components/StartingLocation";
+
+
 
 
 export default function VanDetail() {
-    const inputRef = useRef<HTMLInputElement>(null)
 
-    const { startLocation, setStartLocation } = useLocation()
-
-    async function handleStartSubmit(e: React.FormEvent<HTMLFormElement>) {
-        e.preventDefault()
-
-        console.log(inputRef.current?.value)
-
-        const startPoint = inputRef.current && inputRef.current.value ? inputRef.current.value : "madrid"
-
-
-
-        const { name, latitude, longitude } = await getCoordinates(startPoint)
-        //console.log(name, longitude, latitude)
-        setStartLocation({
-            location: name,
-            lat: latitude,
-            long: longitude
-        })
-
-    }
-
-
-
-    useEffect(() => {
-        const { location, lat, long } = startLocation
-
-        console.log("City:", location, "latitude", lat, "longitude", long);
-    }, [startLocation]);
-
-    const { location, lat, long } = startLocation
-
-    console.log("2nd console", location, lat, long)
-
-
-
+    const { startLocation } = useLocation()
 
     const { vanId } = useParams()
     //console.log(typeof vanId, vanId)
@@ -62,6 +26,7 @@ export default function VanDetail() {
     if (!van) { return <p>No such Van.</p> }
 
     const { description, imageUrl, price } = van
+    const { lat, long } = startLocation
 
     return (
         <Stack direction="vertical" className="van-detail-container">
@@ -79,30 +44,7 @@ export default function VanDetail() {
                     <p>{description} </p>
                     <p>{price}$ per day</p>
 
-                    <Form onSubmit={handleStartSubmit}>
-                        <Form.Group className="mb-3" controlId="formStartLocation">
-                            <Form.Label>Starting Point</Form.Label>
-                            <Form.Control
-                                ref={inputRef}
-                                type="text"
-                                placeholder="Enter start location"
-                                required />
-                            <Form.Text className="text-muted">
-                                I want to start my journey from...
-                            </Form.Text>
-                        </Form.Group>
-
-
-
-                        <Stack direction="horizontal" className="align-items-end justify-content-between">
-                            <Button variant="primary" type="submit">
-                                Submit
-                            </Button>
-                            {
-                                location ? <p className="m-0">Start from: {location}</p> : ""
-                            }
-                        </Stack>
-                    </Form>
+                    <StartingLocation />
 
                 </Stack>
                 <Image src={imageUrl} rounded fluid className="van-img" />
