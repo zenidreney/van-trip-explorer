d3d9c458a73dbd3f64b4d881d1c3b5af13c1a4b3
Deniz <auo@tuta.io>
2025-11-09 17:21:40 +0100

    Polished styles updated links added active state to nav links.

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,15 +1,49 @@
-import { Link } from "react-router"
+import { NavLink } from "react-router"
+
+import Container from 'react-bootstrap/Container';
+import Navbar from 'react-bootstrap/Navbar';
+import Nav from 'react-bootstrap/Nav';
+//import NavDropdown from 'react-bootstrap/NavDropdown';
 
 export default function Header() {
+
+
     return (
         <header>
 
-            <h1>Van-Trip Explorer</h1>
-            <nav>
-                <Link to="/">Home</Link>
-                <Link to="/about">About</Link>
-                <Link to="/vans">Vans</Link>
-            </nav>
+            {/* <h1>Van-Trip Explorer</h1> */}
+            <Navbar expand="lg" className="bg-body-tertiary">
+                <Container>
+                    <Navbar.Brand as={NavLink} to="/" >Van-Trip-Explorer</Navbar.Brand>
+                    <Navbar.Toggle aria-controls="basic-navbar-nav" />
+                    <Navbar.Collapse id="basic-navbar-nav">
+                        <Nav className="me-auto">
+                            <Nav.Link as={NavLink} to="/" >Home</Nav.Link>
+                            <Nav.Link as={NavLink} to="/about" >About</Nav.Link>
+                            <Nav.Link as={NavLink} to="/vans" >Vans</Nav.Link>
+
+                            {/*  
+                           
+                           Maybe add a dropdown later!
+                           
+                           <NavDropdown title="Dropdown" id="basic-nav-dropdown">
+                                <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
+                                <NavDropdown.Item href="#action/3.2">
+                                    Another action
+                                </NavDropdown.Item>
+                                <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
+                                <NavDropdown.Divider />
+                                <NavDropdown.Item href="#action/3.4">
+                                    Separated link
+                                </NavDropdown.Item>
+                            </NavDropdown>
+                                              
+                            */}
+                        </Nav>
+                    </Navbar.Collapse>
+                </Container>
+            </Navbar>
+
         </header>
     )
 }
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -3,7 +3,13 @@ import Badge from 'react-bootstrap/Badge';
 import clsx from 'clsx';
 import "./Home.css"
 
+import { Link } from 'react-router';
+
 import { useVans } from "../hooks/useVans"
+
+import Stack from 'react-bootstrap/Stack';
+import Image from 'react-bootstrap/Image';
+
 
 export default function Home() {
 
@@ -23,17 +29,19 @@ export default function Home() {
         return (
             //<img key={van.id} className="home-vans" src={van.imageUrl} />
             <Carousel.Item key={van.id}>
-                <img
-                    className="d-block w-100"
-                    src={van.imageUrl}
-                    alt="First slide"
-                />
-                <Carousel.Caption>
-                    <h5>{van.name} </h5>
-                    <Badge bg={badgeColor}>
-                        {van.type}
-                    </Badge>
-                </Carousel.Caption>
+                <Link to={`/vans/${van.id}`}>
+                    <Image
+                        className="d-block w-100"
+                        src={van.imageUrl}
+                        alt="First slide"
+                    />
+                    <Carousel.Caption>
+                        <h5>{van.name} </h5>
+                        <Badge bg={badgeColor}>
+                            {van.type}
+                        </Badge>
+                    </Carousel.Caption>
+                </Link>
             </Carousel.Item>
         )
     })
@@ -42,8 +50,18 @@ export default function Home() {
 
 
     return (
-        <Carousel fade className="carousel-container" >
-            {vansElements}
-        </Carousel>
+        <Stack className="align-items-center">
+
+            <h1>Ready for a ride?</h1>
+            <h2>Choose your van</h2>
+            <h2>Choose your route</h2>
+            <Link to="/vans" className="home-link-to-vans mb-2">Ride on</Link>
+            
+
+            <Carousel fade className="carousel-container" >
+                {vansElements}
+            </Carousel>
+
+        </Stack>
     )
 }
