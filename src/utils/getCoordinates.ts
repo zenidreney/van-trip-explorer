bfbb60e065b207ad51af5953feb885741a5c5b22
Deniz <auo@tuta.io>
2025-11-28 14:53:54 +0100

    added error message if no city found

--- src/utils/getCoordinates.ts
+++ src/utils/getCoordinates.ts
@@ -1,15 +1,32 @@
 export async function getCoordinates(location: string) {
-    const res = await fetch(`https://nominatim.openstreetmap.org/search?format=json&q=${location}`, {
-        headers: { 'User-Agent': 'van-trip-explorer-student-project/1.0' }
-    })
+    try {
+        const res = await fetch(`https://nominatim.openstreetmap.org/search?format=json&q=${location}`, {
+            headers: { 'User-Agent': 'van-trip-explorer-student-project/1.0' }
+        })
 
-    const data = await res.json()
-    //console.log(typeof data[0].name, typeof data[0].lat, typeof data[0].lon)
+        if (!res.ok) {
+            console.log(res.status)
+            throw new Error(`Request Error Code: ${res.status} `)
+        }
 
-    return {
-        name: data[0].name,
-        latitude: data[0].lat,
-        longitude: data[0].lon
+        const data = await res.json()
+
+        // console.log(data.length, Array.isArray(data))
+
+        if (!Array.isArray(data) || data.length === 0) {
+            alert("City not found. Please enter a valid city or town in the text box")
+            throw new Error("No results found")
+        }
+
+
+        return {
+            name: data[0].name,
+            latitude: data[0].lat,
+            longitude: data[0].lon
+        }
+    } catch (error) {
+        console.log(error)
+        throw error
     }
-    
+
 }
